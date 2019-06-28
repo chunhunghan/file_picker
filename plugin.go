ee22a8e598b0d2088fdd99119d6a9eb5ed9bbc6f
chunhunghan <chunhunghan@gmail.com>
2019-06-28 15:12:55 +0800

    Create plugin.go

---
+++ plugin.go
@@ -0,0 +1,46 @@
+package file_picker
+
+import (
+	"github.com/go-flutter-desktop/go-flutter"
+	"github.com/go-flutter-desktop/go-flutter/plugin"
+)
+
+const channelName = "file_picker"
+
+type FilePickerPlugin struct{}
+
+var _ flutter.Plugin = &FilePickerPlugin{} // compile-time type check
+
+func (p *FilePickerPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
+	dialogProvider := dialogProvider{}
+
+	channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
+	//channel.HandleFunc("openDirectory", p.filePicker(dialogProvider, true))
+	channel.HandleFunc("openFile", p.filePicker(dialogProvider, false))
+
+	return nil
+}
+
+func (p *FilePickerPlugin) filePicker(dialog dialog, isDirectory bool) func(arguments interface{}) (reply interface{}, err error) {
+	return func(arguments interface{}) (reply interface{}, err error) {
+		/*
+			decodedArgs, ok := arguments.(map[interface{}]interface{})
+			if !ok {
+				return nil, errors.New("arguments must be encoded in JSON format")
+			}
+			title, ok := decodedArgs["title"].(string)
+			if !ok {
+				return nil, errors.New("arguments requires a title parameter with type string")
+			}
+
+			fileDescriptor, _, err := dialog.File(title, "*", isDirectory)
+			if err != nil {
+				return nil, errors.Wrap(err, "failed to open dialog picker")
+			}
+		*/
+
+		fileDescriptor, _, err := dialog.File(title, "*", isDirectory)
+		//fileDescriptor, _, err := dlgs.File("select file", "*", false)
+		return fileDescriptor, nil
+	}
+}
