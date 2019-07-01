3c37e9531ccb337f790fd26f5c4b4e0f1983c309
chunhunghan <chunhunghan@gmail.com>
2019-07-01 13:57:46 +0800

    remove comments

--- plugin.go
+++ plugin.go
@@ -12,12 +12,6 @@ type FilePickerPlugin struct{}
 
 var _ flutter.Plugin = &FilePickerPlugin{} // compile-time type check
 
-/*
-func SayHello() {
-	//fmt.Println("Hello, Ajinkya")
-}
-*/
-
 func (p *FilePickerPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
 	dialogProvider := dialogProvider{}
 
@@ -33,22 +27,11 @@ func (p *FilePickerPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
 
 func (p *FilePickerPlugin) filePicker(dialog dialog, isDirectory bool) func(arguments interface{}) (reply interface{}, err error) {
 	return func(arguments interface{}) (reply interface{}, err error) {
-		/*
-			decodedArgs, ok := arguments.(map[interface{}]interface{})
-			if !ok {
-				return nil, errors.New("arguments must be encoded in JSON format")
-			}
-			title, ok := decodedArgs["title"].(string)
-			if !ok {
-				return nil, errors.New("arguments requires a title parameter with type string")
-			}
-		*/
 		fileDescriptor, _, err := dialog.File("select file", "*", isDirectory)
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to open dialog picker")
 		}
 
-		//fileDescriptor, _, err := dlgs.File("select file", "*", false)
 		return fileDescriptor, nil
 	}
 }
