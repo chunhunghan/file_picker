0d31f8ffaccb3ac67d54534e259dae667e76664c
chunhunghan <chunhunghan@gmail.com>
2019-07-25 15:05:43 +0800

    remove fmt

--- file_linux.go
+++ file_linux.go
@@ -1,7 +1,6 @@
 package file_picker
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -22,7 +21,7 @@ func fileFilter(method string) (string, error) {
 	default:
 		if strings.HasPrefix(method, "__CUSTOM_") {
 			resolveType := strings.Split(method, "__CUSTOM_")
-			filter = `*.` + resolveType[1]			
+			filter = `*.` + resolveType[1]
 		} else {
 			return "", errors.New("unknown method")
 		}
